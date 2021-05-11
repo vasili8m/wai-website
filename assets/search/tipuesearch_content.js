8f4d6eb4f246402abbf83f2fc8495fb7193fc053
shawna-slh <shawna-slh@users.noreply.github.com>
2021-05-11 18:44:21 +0000

    deploy: f76dcaa1b575a3720416cbeae2a60c58dbda02ef

--- assets/search/tipuesearch_content.js
+++ assets/search/tipuesearch_content.js
@@ -695,7 +695,7 @@ var tipuesearch = {"pages": [{
     "url": "/WAI/fundamentals/accessibility-principles/changelog/"
   },{
     "title": "Changelog for Evaluating Web Accessibility Overview",
-    "text": "## May 2021 * Added text in English to the Spanish translation.",
+    "text": "{::nomarkdown} {% include box.html type=\"start\" title=\"About\" class=\"\" %} {:/} This page is designed to help update translations. {::nomarkdown} {% include box.html type=\"end\" %} {:/} {::nomarkdown} {% include toc.html type=\"start\" title=\"Page Contents\" %} {:/} - This will be replaced by an automatically generated TOC when using Markdown formatting. {::options toc_levels=\"2,3\" /} {:toc} {::nomarkdown} {% include toc.html type=\"end\" %} {:/} _Translators:_ * Change `last_updated` to the date of the translation update. * The other dates after `footer >` should match the English version. * From the GitHub pull request, after a couple of minutes, you can get a preview of your rendered page by selecting the \"Details\" link after \"netlify/wai-.../deploy-preview — Deploy preview ready!\". Then select your language at the top. ## 7 May 2021 * Added changelog. [Diff of markup](https://github.com/w3c/wai-eval-overview/commit/4843dca9aba0d63b044cb4c784cdf0b4b8b75984#diff-a4f40b84fbcf1a7939d596dca4939bbae6e56c05ad06a62b719b42693a9baf07) * Added new text in English to the Spanish translation.",
     "tags": "",
     "url": "/WAI/test-evaluate/changelog/"
   },{
